113a857a5796dacf23ef81acfcac1f6f1c0b4abf
thomascsd <thomascsd808@gmail.com>
2023-04-05 17:18:43 +0800

    feat(shared): add new layout component with header and footer components feat(shared): update header component with navigation links feat(shared): update footer component with twitter icon in svg format

--- src/app/shared/header.component.ts
+++ src/app/shared/header.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  template: ``,
+  imports: [RouterModule],
+  template: `<nav class="d-flex flex-row">
+    <a class="ms-auto" href="/">Space Viewer</a>
+    <a [routerLink]="['/pictureOfDay']" routerLinkActive="active">Picutre of day</a>
+    <a [routerLink]="['/spacePictures']" routerLinkActive="active">Space Pictures</a>
+  </nav>`,
+  styles: [
+    `
+      nav {
+        border-top: 6px solid #40aff4;
+      }
+    `,
+  ],
 })
 export class HeaderComponent {}
---
+++ src/app/shared/layout.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header.component';
+import { FooterComponent } from './footer.component';
+
+@Component({
+  standalone: true,
+  imports: [HeaderComponent, FooterComponent, RouterModule],
+  template: `<div class="d-flex flex-column">
+    <app-header></app-header>
+    <main class="container flex-grow-1">
+      <router-outlet></router-outlet>
+    </main>
+    <app-footer></app-footer>
+  </div> `,
+  styles: [``],
+})
+export class LayoutComponent {}
