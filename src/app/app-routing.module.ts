2cb717ab83aa48f3172790dae1cccef25c17d73a
Thomas Chang <thomascsd808@gmail.com>
2023-04-08 09:43:43 +0800

    feat(app-routing.module.ts): add routes for pictureOfDay and spacePictures components feat(space-pictures.component.ts): create SpacePicturesComponent with empty template test(space-pictures.component.spec.ts): add test for SpacePicturesComponent creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './shared/layout.component';
+import { PictureOfDayComponent } from './pictureOfDay/picture-of-day.component';
+import { SpacePicturesComponent } from './space-pictures/space-pictures.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'pictureOfDay', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'pictureOfDay', component: PictureOfDayComponent },
+      { path: 'spacePictures', component: SpacePicturesComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/space-pictures/space-pictures.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SpacePicturesComponent } from './space-pictures.component';
+
+describe('SpacePicturesComponent', () => {
+  let component: SpacePicturesComponent;
+  let fixture: ComponentFixture<SpacePicturesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ SpacePicturesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SpacePicturesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
