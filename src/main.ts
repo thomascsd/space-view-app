a1bac4bc0275fb0da815e09af08499fd9409b295
Thomas Chang <thomascsd808@gmail.com>
2023-04-20 23:04:37 +0800

    refactor(app): remove unused files and modules refactor(app): simplify app component template refactor(main.ts): use bootstrapApplication instead of bootstrapModule and importProvidersFrom to import providers

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideHttpClient()
+    ]
+})
   .catch(err => console.error(err));
