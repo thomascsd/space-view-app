84fd50edc41e23104eadc6df8a2e0d14e85fa159
Thomas Chang <thomascsd808@gmail.com>
2023-06-02 22:39:49 +0800

    feat(picture-card.component.ts): add NgOptimizedImage to optimize image loading style(picture-card.component.ts): format code and add line breaks for readability

--- src/app/shared/components/picture-card.component.ts
+++ src/app/shared/components/picture-card.component.ts
@@ -1,10 +1,19 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-picture-card',
   standalone: true,
+  imports: [NgOptimizedImage],
   template: ` <div class="card">
-    <img src="{{ imageSrc }}" class="card-img-top" alt="{{ title }}" />
+    <img
+      [ngSrc]="imageSrc"
+      [width]="200"
+      [height]="100"
+      loading="lazy"
+      class="card-img-top"
+      alt="{{ title }}"
+    />
     <div class="card-body">
       <h2 class="card-title fs-4">{{ title }}</h2>
       <p class="card-text fs-5">{{ description }}</p>
