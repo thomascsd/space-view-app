bc7dc7751d416d2991fa7ac0e90f5b8551e14569
Thomas Chang <thomascsd808@gmail.com>
2024-09-23 00:30:41 +0800

    add pageable.min.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+  templateUrl: './app.component.html',
   standalone: true,
   imports: [RouterOutlet],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'space-view-app';
+
+  ngAfterViewInit(): void {
+    new Pageable('#container', {
+      childSelector: '[data-anchor]', // CSS3 selector string for the pages
+      anchors: [], // define the page anchors
+      pips: true, // display the pips
+      animation: 300, // the duration in ms of the scroll animation
+      freeScroll: false, // allow manual scrolling when dragging instead of automatically moving to next page
+      events: {
+        wheel: true, // enable / disable mousewheel scrolling
+        mouse: true, // enable / disable mouse drag scrolling
+        touch: true, // enable / disable touch / swipe scrolling
+        keydown: true, // enable / disable keyboard navigation
+      },
+      onInit: function () {
+        // do something when the instance is ready
+      },
+      onUpdate: function () {
+        // do something when the instance updates
+      },
+      onBeforeStart: function () {
+        // do something before scrolling begins
+      },
+      onStart: function () {
+        // do something when scrolling begins
+      },
+    });
+  }
 }
